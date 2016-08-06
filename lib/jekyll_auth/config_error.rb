e0f9b6d22fbb85a3fc0018c93e20d634c132b387
dgreboval <damiengreboval@gmail.com>
2016-08-06 22:39:47 +0200

    add gemfile jekyll-auth, bundle install

--- lib/jekyll_auth/config_error.rb
+++
@@ -1,7 +0,0 @@
-class JekyllAuth
-  class ConfigError < SecurityError
-    def message
-      'Jekyll Auth is refusing to serve your site because your oauth credentials are not properly configured.'
-    end
-  end
-end
--- lib/jekyll_auth/version.rb
+++
@@ -1,3 +0,0 @@
-class JekyllAuth
-  VERSION = '2.0.0'.freeze
-end
